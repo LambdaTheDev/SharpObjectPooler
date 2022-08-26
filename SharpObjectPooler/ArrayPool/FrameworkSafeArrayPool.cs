168f913c0e91b034293fba9a4b62aa91c0128721
Lambda <34376990+LambdaTheDev@users.noreply.github.com>
2022-08-26 20:24:59 +0200

    Started implementing .NET 2 ArrayPool

---
+++ SharpObjectPooler/ArrayPool/FrameworkSafeArrayPool.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+#if !NETSTANDARD2_0
+using System.Buffers;
+#endif
+
+namespace LambdaTheDev.SharpObjectPooler.ArrayPool
+{
+    // Simple utility that wraps C#'s ArrayPool if possible, or uses my custom implementation
+    public static class FrameworkSafeArrayPool<T>
+    {
+#if NETSTANDARD2_0
+        // If .NET Standard, use my implementation
+        private static readonly Net2ArrayPoolImpl<T> Pool = new Net2ArrayPoolImpl<T>();
+#endif
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T[] Rent(int length)
+        {
+#if NETSTANDARD2_0
+            return Pool.Rent(length);
+#else
+            return ArrayPool<T>.Shared.Rent(length);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Return(T[] array)
+        {
+#if NETSTANDARD2_0
+            Pool.Return(array);
+#else
+            ArrayPool<T>.Shared.Return(array);
+#endif
+        }
+    }
+}
---
+++ SharpObjectPooler/ArrayPool/Net2ArrayPoolImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LambdaTheDev.SharpObjectPooler.ArrayPool
+{
+#if NETSTANDARD2_0
+    
+    // My simple ArrayPool implementation
+    internal class Net2ArrayPoolImpl<T>
+    {
+        public T[] Rent(int size)
+        {
+            return Array.Empty<T>();
+        }
+
+        public void Return(T[] array)
+        {
+            
+        }
+    }
+    
+#endif
+}
